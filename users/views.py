5b07693a41e6fb63d0f3fcb64b7ed51cffdf500e
Taras265 <70756830+Taras265@users.noreply.github.com>
2022-11-07 22:01:22 +0200

    Register update

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 import random
 
+from django.contrib import messages
 from django.shortcuts import render
 from django.views.generic import View
 
@@ -10,6 +11,7 @@ from users.models import LibraryUser, Reader
 class RegisterView(View):
     template_name = 'form.html'
     form_class = forms.ReaderRegisterForm
+    success_message = "Користувач зареєстрован"
 
     def get(self, request):
         form = self.form_class()
@@ -20,26 +22,25 @@ class RegisterView(View):
 
     def post(self, request):
         form = self.form_class(request.POST)
-        if form.is_valid():
-            data = form.cleaned_data
+        try:
+            if form.is_valid():
+                data = form.cleaned_data
 
-            qs = Reader.objects.get(card_number=form.data.get('reader_card_number'))
-            data['reader'] = qs.id
+                qs = Reader.objects.get(card_number=form.data.get('reader_card_number'))
+                data['reader'] = qs.id
 
-            user = LibraryUser.objects.create_user(data['username'], data['email'], data['password'])
-            user.is_active = False
-            user.first_name = qs.first_name
-            user.second_name = qs.second_name
-            user.last_name = qs.last_name
-            user.email_code = random.randint(100000, 999999)
-            user.reader = qs
-            user.user_type = 'Reader'
+                user = LibraryUser.objects.create_user(data['username'], data['email'], data['password'])
+                user.is_active = False
+                user.first_name = qs.first_name
+                user.second_name = qs.second_name
+                user.last_name = qs.last_name
+                user.email_code = random.randint(100000, 999999)
+                user.reader = qs
+                user.user_type = 'Reader'
 
-            user.save()
+                user.save()
+        except ValueError as e:
+            messages.error(request, e)
 
-            message = "Success"
-        else:
-            message = "Error"
         return render(request, self.template_name, context={'form': form,
-                                                            'message': message,
                                                             'button': 'Зареєструватись'})
